cb1bc9912711aa461a69ef7bab31b19ab9ed8182
tiger <wanhu88@gmail.com>
2015-07-09 23:17:27 +0800

    迭代

--- bin/index.js
+++ bin/index.js
@@ -1,60 +1,29 @@
 #!/usr/bin/env node
+
 var fs = require('fs');
 var fse = require('fs-extra');
 var path = require('path');
 var spawn = require('child_process').spawn;
 var async = require('async');
+
 var args = process.argv.slice(2);
-if (args.length === 0) {
-    console.error(
-        'You did not pass any commands, did you mean to run `pandorajs init`?'
-    );
+
+var program = require('commander');
+
+if (!process.argv.slice(2).length) {
+    console.error('你没有输入任何命令，是否想输入`pandorajs init`?\n你可以通过 pandorajs -h 获得更信息');
     process.exit(1);
 }
 
-if (args[0] === 'init') {
-    init(args[1]);
-} else {
-    process.exit(1);
-}
-
-
-var ipt = 'copy';
-
-function init() {
-    try {
-        process.stdin.on('readable', function() {
-            var chunk = process.stdin.read();
-            if (!chunk) {
-                return
-            }
-            chunk = chunk.toString('utf8', 0, chunk.length).replace('\n', '');
-            if (!chunk || chunk.toLowerCase() == 'y' || chunk.toLowerCase() == 'yes') {
-                switch(ipt){
-                    case 'copy':
-                        copyFiles(1);
-                        break;
-                    case 'start':
-                        startServer(1);
-                        break;
-                }
-            } else {
-                process.exit(1);
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        return;
-    }
-    copyFiles();
-}
-
-function copyFiles(force) {
-    ipt = 'copy';
-    try {
-        var isEmpty = fs.readdirSync(process.cwd()).length === 0;
-        if (!isEmpty && !force) {
-            process.stdout.write('目录下有文件,继续操作可能覆盖已有文件。是否继续?(yes):');
+program
+    .command('init')
+    .description('初始化项目')
+    .option('-s, --start [value]', '是否提示开启服务', function(val) {
+        return val != 'no' && val != 'false';
+    }, true)
+    .action(function(cmd) {
+        var ipt = 'copy';
+        try {
             process.stdin.on('readable', function() {
                 var chunk = process.stdin.read();
                 if (!chunk) {
@@ -62,26 +31,72 @@ function copyFiles(force) {
                 }
                 chunk = chunk.toString('utf8', 0, chunk.length).replace('\n', '');
                 if (!chunk || chunk.toLowerCase() == 'y' || chunk.toLowerCase() == 'yes') {
-                    copyFiles(1);
+                    switch (ipt) {
+                        case 'copy':
+                            copyFiles(1);
+                            break;
+                        case 'start':
+                            startServer(1);
+                            break;
+                    }
                 } else {
                     process.exit(1);
                 }
             });
-        } else {
-            var flist = fs.readdirSync(path.join(__dirname, 'demo'));
-            console.log('正在创建文件...');
-            flist.forEach(function(f) {
-                fse.copySync(path.join(__dirname, 'demo', f), path.join(process.cwd(), f));
-            });
-            console.log('创建完成！');
-            startServer();
+        } catch (error) {
+            console.error(error);
+            return;
         }
+        copyFiles();
 
-    } catch (error) {
-        console.error(error);
-        return;
-    }
-}
+        function copyFiles(force) {
+            ipt = 'copy';
+            try {
+                var isEmpty = fs.readdirSync(process.cwd()).length === 0;
+                if (!isEmpty && !force) {
+                    process.stdout.write('目录下有文件,继续操作可能覆盖已有文件。是否继续?(yes):');
+                    process.stdin.on('readable', function() {
+                        var chunk = process.stdin.read();
+                        if (!chunk) {
+                            return
+                        }
+                        chunk = chunk.toString('utf8', 0, chunk.length).replace('\n', '');
+                        if (!chunk || chunk.toLowerCase() == 'y' || chunk.toLowerCase() == 'yes') {
+                            copyFiles(1);
+                        } else {
+                            process.exit(1);
+                        }
+                    });
+                } else {
+                    var fpath = path.dirname(__dirname);
+                    var flist = fs.readdirSync(path.join(fpath, 'demo'));
+                    console.log('正在初始化...');
+                    flist.forEach(function(f) {
+                        fse.copySync(path.join(fpath, 'demo', f), path.join(process.cwd(), f));
+                    });
+                    // console.log('__dirname',__dirname);
+                    run('npm install', null, function() {
+                        // code
+                        // console.log('创建完成！');
+                        cmd.start && startServer();
+                    });
+                }
+
+            } catch (error) {
+                console.error(error);
+                return;
+            }
+        }
+    });
+
+program
+    .command('start')
+    .description('启动服务')
+    .action(function(opt) {
+        startServer(1);
+    });
+
+program.parse(process.argv);
 
 function startServer(force) {
     ipt = 'start';
@@ -89,8 +104,8 @@ function startServer(force) {
         if (!force) {
             process.stdout.write('现在就要运行网站服务吗?(yes):');
         } else {
-            
-            run('node start.js', function(){
+
+            run('node start.js', null, function() {
                 // code
             });
         }
@@ -100,7 +115,7 @@ function startServer(force) {
     }
 }
 
-function run(command, cb) {
+function run(command, opt, cb) {
     var parts = command.split(/\s+/);
     var cmd = parts[0];
     var args = parts.slice(1);
